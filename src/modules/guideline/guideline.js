436339c4deb5867389a4050c4b4a2cae900759b5
alirezamirian <alireza.mirian@gmail.com>
2015-04-21 01:15:09 +0430

    docs(guideline): add basic docs for guideline module

--- src/modules/guideline/guideline.js
+++ src/modules/guideline/guideline.js
@@ -4,9 +4,40 @@
  */
 
 
+/**
+ * @ngdoc module
+ * @name visular.guideline
+ * @description
+ * This module provide semantic and UI for adding a guideline system to vz-designer.
+ * Two major part of the module are `vzGuideline` directive, and `VzGuidelineFactory` service.
+ *
+ * `vzGuidelineFactory` is a service with a `create()` method, which creates a guideline system object
+ *  that handles all positioning semantics for a guideline system.
+ *
+ *  `vzGuideline` directive makes use of vzDesigner controller api to register a **drag interceptor**
+ *  for the designer. It also adds an overlay to designer for visualizing guidelines
+ */
 angular.module("visular.guideline", [])
 
 
+/**
+ * @ngdoc directive
+ * @name vzGuideline
+ * @module visular.guideline
+ *
+ * @restrict A
+ *
+ * @description
+ * `vz-guideline` is a directive that adds **guidelines** to `vz-designer` element that help neat alignment of the
+ * elements when dragging them. It internally uses `vzGuidelineFactory` which is a service that provides all
+ * positioning semantics of moving rects in such guideline-enabled system.
+ *
+ * @usage
+ * <hljs lang="html">
+ *  <vz-designer vz-guideline>
+ *  </<vz-designer>
+ * </hljs>
+ */
     .directive("vzGuideline", function vzGuidelineDirective(VzGuidelineFactory){
         var overlayTemplate =
             '<div class="vz-guideline"' +
@@ -55,6 +86,29 @@ angular.module("visular.guideline", [])
             }
         }
     })
+/**
+ * @ngdoc service
+ * @name VzGuidelineFactory
+ * @module visular.guideline
+ * @description
+ * ##NOTE: you, as a user of vz-guideline, don't need to know about what goes on under the hood in `VzGuidelineFactory`.
+ * it's usage is limited to `vzGuideline` directive. So you can freely skip this documentation, unless you want to write
+ * your own implementation of the guideline directive, based on `vzGuidelineFactory`.
+ *
+ * If you want to encapsulate all semantics of a guideline system into a model, it should:
+ * * Let you specify position of a rectangle inside a coordinate system
+ * * Give you back the guided position of that rectangle
+ * * Tell you what guidelines are utilized (if any) to compute guided position
+ *
+ * `create()` method of `VzGuidelineFactory` is responsible for creating such models.
+ * It accepts three arguments:
+ *  * containerRect: The rect which specifies the container boundries.
+ *  * otherRects: An array of rects that are used to adding guidelines based on their positions
+ *  * targetRect: The target rect that its position should be mapped to a guided position
+ *
+ *
+ *  // TODO: complete docs
+ */
     .factory("VzGuidelineFactory", function(){
         var Guideline = {
             create: function(containerRect, otherRects, targetRect){
