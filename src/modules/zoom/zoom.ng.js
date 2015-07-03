f5adc9a31e57a672a9610bf0cecebb10a2db6878
alirezamirian <alireza.mirian@gmail.com>
2015-07-04 00:15:30 +0430

    fix(zoom): compute delta in wheel event in a more consistent way

--- src/modules/zoom/zoom.ng.js
+++ src/modules/zoom/zoom.ng.js
@@ -10,25 +10,53 @@
 
 
     function vzZoomDirectiveFactory(){
-        var deltaNormalizer = .1;
+        var zoomScaleSensitivity = .1;
         return{
             restrict: "A",
             require: "vzDiagram",
             link: function(scope, elem, attrs, vzDiagramCtrl){
 
+                var lastMouseWheelEventTime;
                 scope.$on("$destroy", cleanUp);
                 init();
                 function wheelHandler(event){
                     // Inspired by https://github.com/ariutta/svg-pan-zoom
-                    var point = getEventPoint(event, vzDiagramCtrl.rootSvgElem),
+                    var evt = event.originalEvent,
+                        point = getEventPoint(event, vzDiagramCtrl.rootSvgElem),
                         oldCTM = vzDiagramCtrl.getTransformationMatrix(),
-                        relativePoint = point.matrixTransform(oldCTM.inverse()),
-                        delta = event.originalEvent.deltaY,
-                        newZoom = oldCTM.a - delta*deltaNormalizer,
-                        modifier = vzDiagramCtrl.rootSvgElem[0].createSVGMatrix()
-                            .translate(relativePoint.x, relativePoint.y)
-                            .scale(newZoom/oldCTM.a)
-                            .translate(-relativePoint.x, -relativePoint.y);
+                        relativePoint = point.matrixTransform(oldCTM.inverse());
+
+
+                    // Default delta in case that deltaY is not available
+                    var delta = evt.deltaY || 1
+                        , timeDelta = Date.now() - lastMouseWheelEventTime
+                        , divider = 3 + Math.max(0, 30 - timeDelta);
+
+                    // Update cache
+                    lastMouseWheelEventTime = Date.now();
+
+                    // Make empirical adjustments for browsers that give deltaY in pixels (deltaMode=0)
+                    if ('deltaMode' in evt && evt.deltaMode === 0 && evt.wheelDelta) {
+                        delta = evt.deltaY === 0 ? 0 :  Math.abs(evt.wheelDelta) / evt.deltaY;
+                    }
+
+                    delta = -0.3 < delta && delta < 0.3 ? delta : (delta > 0 ? 1 : -1) * Math.log(Math.abs(delta) + 10) / divider;
+
+                    var newRelativeZoom = Math.pow(1 + zoomScaleSensitivity, (-1) * delta);
+
+                    var modifier = vzDiagramCtrl.rootSvgElem[0].createSVGMatrix()
+                        .translate(relativePoint.x, relativePoint.y)
+                        .scale(newRelativeZoom)
+                        .translate(-relativePoint.x, -relativePoint.y);
+                    // Update cache
+                    lastMouseWheelEventTime = Date.now();
+
+                    // Make empirical adjustments for browsers that give deltaY in pixels (deltaMode=0)
+                    if ('deltaMode' in event && evt.deltaMode === 0 && evt.wheelDelta) {
+                        delta = evt.deltaY === 0 ? 0 :  Math.abs(evt.wheelDelta) / evt.deltaY
+                    }
+
+                    delta = -0.3 < delta && delta < 0.3 ? delta : (delta > 0 ? 1 : -1) * Math.log(Math.abs(delta) + 10) / divider
                     console.log(delta);
                     var newCTM = oldCTM.multiply(modifier);
                     scope.$apply(function(){
@@ -56,6 +84,7 @@
                 }
                 function init(){
                     elem.bind("wheel", wheelHandler);
+                    lastMouseWheelEventTime = Date.now();
                 }
                 function cleanUp(){
                     elem.unbind("wheel", wheelHandler);
