5c238c02ffa4ef9968440225e366fbf0a5dd2421
alirezamirian <alireza.mirian@gmail.com>
2015-07-10 20:29:49 +0430

    refactor(element): refactor element directive

--- src/modules/core/element/element.js
+++ src/modules/core/element/element.js
@@ -32,7 +32,7 @@
             this.rotation = 0;
         }
     }
-    function vzElementDirective(vzConfig, $document, VzDraggableFactory, $timeout, VZ_THRESHOLD){
+    function vzElementDirective(vzConfig, VzDraggableFactory){
         return{
 
             restrict: "A",
@@ -41,54 +41,44 @@
                 model: "=vzElement"
             },
             template:
-            '<svg class="vz-element" \
-                ng-class="{\'vz-selected\': vzDiagram.isSelected(model)}"\
-                ng-attr-width="{{model.size.width}}" \
-                ng-attr-height="{{model.size.height}}" \
-                ng-attr-x="{{model.position.x}}" \
-                ng-attr-y="{{model.position.y}}" \
-                ng-include="markupUrl()">\
-            </svg>',
+                '<svg class="vz-element" \
+                    ng-class="{\'vz-selected\': vzDiagram.isSelected(model)}"\
+                    ng-attr-width="{{model.size.width}}" \
+                    ng-attr-height="{{model.size.height}}" \
+                    ng-attr-x="{{model.position.x}}" \
+                    ng-attr-y="{{model.position.y}}" \
+                    ng-include="markupUrl()">\
+                </svg>',
             link: function(scope, elem, attrs, diagramController){
                 scope.vzDiagram = diagramController;
                 scope.markupUrl = function(){
                     return vzConfig.markupPath + "/" + scope.model.type + ".svg";
                 };
 
-                // control scales
-                scope.scaleX = 1;
-                scope.scaleY = 1;
-                scope.$watch(function () {
-                    return elem.get(0).getBBox().height.toFixed(1);
-                }, function (newValue, oldValue) {
-                    console.log(oldValue + " => " + newValue);
-                });
-                /*scope.$watch(function(){
-                 return +elem.get(0).getBBox().height.toFixed(1);
-                 }, function(height){
-                 if(height && Math.abs(scope.model.size.height - height) > VZ_THRESHOLD)
-                 scope.scaleY = scope.model.size.height / height ;
-                 });
-                 scope.$watch(function(){
-                 return +elem.get(0).getBBox().width.toFixed(1);
-                 }, function(width){
-                 if(width && Math.abs(scope.model.size.width - width) > VZ_THRESHOLD)
-                 scope.scaleX = scope.model.size.width / width;
-                 });*/
-                /*scope.$watch(function(){
-                 var currentBBox = elem.get(0).getBBox();
-                 console.log("currBbox", currentBBox);
-                 console.log(Math.abs(scope.model.size.height - currentBBox.height),Math.abs(scope.model.size.width - currentBBox.width))
+                init();
+                scope.$on("$destroy", cleanup);
 
-                 $timeout(function(){
-                 if(currentBBox.height && Math.abs(scope.model.size.height - currentBBox.height) > VZ_THRESHOLD)
-                 scope.scaleY = scope.model.size.height / currentBBox.height ;
-                 if(currentBBox.width && Math.abs(scope.model.size.width - currentBBox.width) > VZ_THRESHOLD)
-                 scope.scaleX = scope.model.size.width / currentBBox.width;
-                 },10)
-
-
-                 });*/
+                function init(){
+                    elem.bind("mousedown", mousedown);
+                    elem.bind("mouseup", mouseup);
+                    new VzDraggableFactory(elem, diagramController.elem)
+                        .onDragStart(function(evt){
+                            diagramController.drag.started(scope.model, evt);
+                        })
+                        .onDragFinish(function(evt){
+                            diagramController.drag.finished(scope.model, evt);
+                        })
+                        .onDrag(function(evt){
+                            diagramController.drag
+                                .to(
+                                diagramController.relativePoint(g.point(this.draggedPosition.x,this.draggedPosition.y)),
+                                scope.model, evt);
+                        });
+                }
+                function cleanup(){
+                    elem.unbind("mouseup", mouseup);
+                    elem.unbind("mousedown", mousedown);
+                }
                 function mousedown(){
                     scope.$apply(function(){
                         diagramController.bringToFront(scope.model);
@@ -99,28 +89,6 @@
                         diagramController.select(scope.model);
                     });
                 }
-
-                elem.bind("mousedown", mousedown);
-                scope.$on("$destroy", function(){
-                    elem.unbind("mousedown", mousedown);
-                });
-
-                elem.bind("mouseup", mouseup);
-                scope.$on("$destroy", function(){
-                    elem.unbind("mouseup", mouseup);
-                });
-
-                new VzDraggableFactory(elem, diagramController.elem)
-                    .onDragStart(function(evt){
-                        diagramController.drag.started(scope.model, evt);
-                    })
-                    .onDragFinish(function(evt){
-                        diagramController.drag.finished(scope.model, evt);
-                    })
-                    .onDrag(function(evt){
-                        diagramController.drag
-                            .to(diagramController.relativePoint(g.point(this.draggedPosition.x,this.draggedPosition.y)), scope.model, evt);
-                    });
             }
         }
     }
